87f892675f1ffa9d6ec43861e276f0ddba376c2a
tin-to-mog <tin.to.mog@gmail.com>
2017-02-04 17:03:26 -0500

    Added dependencies and TypeScript config. Also added gitignore file.

---
+++ tsconfig.js
@@ -0,0 +1,17 @@
+{
+    "compilerOptions": {
+        "allowJs": true,
+        "target": "es5",
+        "module": "commonjs",
+        "moduleResolution": "node",
+        "emitDecoratorMetadata": true,
+        "experimentalDecorators": true,
+        "lib": [ "es2015", "dom" ],
+        "noImplicitAny": true,
+        "outDir": "./built/",
+        "types": [
+            "jasmine",
+            "node"
+        ]
+    }
+}
